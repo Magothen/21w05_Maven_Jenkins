699a5ee3ef6ee9df1428366f1b98713982a3a3bb
Maria <magothen@gmail.com>
2021-02-05 14:06:24 +0100

    första commit från desktop

---
+++ src/test/java/testFoo.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class testFoo {
+
+    @Test
+    void myMethod() {
+        Assertions.assertTrue(Foo.myMethod());
+    }
+}
