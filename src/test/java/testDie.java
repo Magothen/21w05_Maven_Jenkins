20c32001673a83610f4a928e4365c08cfb1a150b
Maria <magothen@gmail.com>
2021-02-05 16:09:24 +0100

    kört alla tester med JUnit nu och bytt param-fil till den från desktop med version 5.4.2 (var 5.7 från maven)

--- src/test/java/testDie.java
+++ src/test/java/testDie.java
@@ -1,21 +1,19 @@
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 
 public class testDie {
-
     //test här
     @ParameterizedTest
     @ValueSource(ints = {4,6,8,10,12,20,100})
     void testDieClass(int input){
         Die testDie = new Die(input); //test code
-        Boolean actual = false;
+        boolean actual = false;
         testDie.roll(); //CUT
         if(testDie.getValue() <= input){
             actual = true;
         }
         Assertions.assertTrue(actual);
     }
-
 }
